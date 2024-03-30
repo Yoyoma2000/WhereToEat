4674994b56f33559a05467d53a337651b2d074b9
alanwu <alan920@students.cs.ubc.ca>
2024-03-30 03:16:21 -0700

    Phase 4: Task 2

---
+++ src/test/model/event/EventTest.java
@@ -0,0 +1,51 @@
+package model.event;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the Event class
+ */
+public class EventTest {
+	private Event e;
+	private Date d;
+
+    private Event e1;
+    private Date d1;
+	
+	//NOTE: these tests might fail if time at which line (2) below is executed
+	//is different from time that line (1) is executed.  Lines (1) and (2) must
+	//run in same millisecond for this test to make sense and pass.
+	
+	@BeforeEach
+	public void runBefore() {
+		e = new Event("Added new restaurant");   // (1)
+		d = Calendar.getInstance().getTime();   // (2)
+
+        e1 = new Event("Searched restaurant");
+        d1 = Calendar.getInstance().getTime();
+	}
+	
+	@Test
+	public void testEvent() {
+		assertEquals("Added new restaurant", e.getDescription());
+		assertEquals(d, e.getDate());
+	}
+
+	@Test
+	public void testToString() {
+		assertEquals(d.toString() + "\n" + "Added new restaurant", e.toString());
+	}
+
+    @Test
+    public void testEquals() {
+        assertFalse(e.equals(e1));
+        assertTrue(e.equals(e));
+    }
+}
